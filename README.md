78081de7ba1156e3635ab0d67aa68412d395b2d3
Ali İhsan Şenel <aliihsansenel0@gmail.com>
2021-11-08 03:59:14 +0300

    add spacing options editing functionality and panel for columns, add style

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # Bootstrap 5 Builder
 
-A React application providing low-code platform to practising Bootstrap's layout and grid concepts. Add new rows or columns, click columns to edit their grid options.
+A React application providing low-code platform to practising Bootstrap's layout and grid concepts. Add new rows or columns, click columns to edit their grid and spacing options.
 
 ## Live Demo
 
 Open [live demo](https://bootstrap-builder.netlify.app/), hosted on Netlify.
+
+## Future Commits
+
+* seperate classes
+* migrate to sass
+* ...
+* migrate to HOC pattern
--- src/bootstrap-ui/pages/builder/comps/Col.js
+++ src/bootstrap-ui/pages/builder/comps/Col.js
@@ -2,43 +2,75 @@ import React, { useState, useEffect, useContext, useRef } from "react";
 
 import plus from "../../../../assets/plus-solid.svg";
 
-import { ClassSelectorContext } from "../BuilderContainer";
+import { ClassSelectorContext, buildModes } from "../BuilderContainer";
+import { breakPointOptions } from "./ClassSelector";
 
 import "./style.css";
 
 export default function Col() {
     const [classes, setClasses] = useState({
         grid: ["col-12", "col-sm-6", "col-lg-4"],
-        spacing: ["mb-2"],
+        spacing: { m: ["mb-2"], p: ["p-1"] },
     });
-    const { rawClassesData, setClassesState, setDisplayClassSelector } =
-        useContext(ClassSelectorContext);
-    let rawClasses = useRef({ grid: ["12", "6", "4", "4", "4", "4"] });
+    const {
+        rawClassesData,
+        setClassesState,
+        setDisplayClassSelector,
+        buildMode,
+    } = useContext(ClassSelectorContext);
+    let rawClasses = useRef({
+        grid: ["12", "6", "4", "4", "4", "4"],
+        spacing: getDefaultSpacingData(),
+    });
     function clickHandler(e) {
         setDisplayClassSelector((displayClassSelector) => {
             if (!displayClassSelector) {
                 setClassesState.current = setClasses;
-                rawClassesData.current.grid = rawClasses.current.grid;
+                rawClassesData.current = rawClasses.current;
                 return true;
             }
             return false;
         });
     }
+
     const classesString = useRef("");
+    const allClassesString = useRef("");
 
     function getClassesString() {
-        classesString.current = classes.grid.join(" ") + " ";
-        classesString.current += classes.spacing.join(" ");
+        switch (buildMode) {
+            case buildModes.GRID:
+                classesString.current = " " + classes.grid.join(" ");
+                break;
+            case buildModes.SPACING:
+                classesString.current = "";
+                if (classes.spacing["m"].length != 0)
+                    classesString.current +=
+                        " " + classes.spacing["m"].join(" ");
+                if (classes.spacing["p"].length != 0)
+                    classesString.current +=
+                        " " + classes.spacing["p"].join(" ");
+                break;
+            default:
+                classesString.current = " " + classes.grid.join(" ");
+                break;
+        }
         return classesString.current;
     }
-
+    function getAllClassesString() {
+        allClassesString.current = classes.grid.join(" ");
+        if (classes.spacing["m"].length != 0)
+            allClassesString.current += " " + classes.spacing["m"].join(" ");
+        if (classes.spacing["p"].length != 0)
+            allClassesString.current += " " + classes.spacing["p"].join(" ");
+        return allClassesString.current;
+    }
     return (
-        <div className={getClassesString()} onClick={clickHandler}>
+        <div className={getAllClassesString()} onClick={clickHandler}>
             <div
-                className="p-1"
+                className="noselect p-1"
                 style={{ wordSpacing: "0.3em", letterSpacing: "0.05em" }}
             >
-                {"." + classesString.current.replaceAll(" ", " .")}
+                {getClassesString().replaceAll(" ", " .")}
             </div>
         </div>
     );
@@ -46,7 +78,7 @@ export default function Col() {
 
 function AddCol({ clickHandler }) {
     return (
-        <div className="add-col w-100 d-flex justify-content-center">
+        <div className="add-col w-100 d-flex justify-content-center noselect">
             <div
                 className="col-5 col-md-3 text-center d-flex justify-content-center align-items-center"
                 onClick={clickHandler}
@@ -62,3 +94,20 @@ function AddCol({ clickHandler }) {
     );
 }
 export { AddCol };
+
+function getBlankSpacingData(sides) {
+    const _bp = Array(breakPointOptions.length).fill(sides);
+    return _bp;
+}
+function getDefaultSpacingData() {
+    let marginArray = getBlankSpacingData(["_", "_", "_", "2"]);
+    let paddingArray = getBlankSpacingData(["1", "1", "1", "1"]);
+    
+    return [marginArray, paddingArray];
+}
+function _getBlankSpacingData() {
+    const _4 = Array(4).fill("_");
+    const _bp = Array(breakPointOptions.length).fill(_4);
+    const _mp = Array(2).fill(_bp);
+    return _mp;
+}
